b40a204ba32be5037b67eda74576b2536efdc966
amschel99 <6290.2020@students.ku.ac.ke>
2024-01-18 11:47:39 +0300

    import necessary crates

---
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -0,0 +1,10 @@
+use serde;
+use candid::{Decode, Encode};
+use ic_cdk::api::time;
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
+use std::{borrow::Cow, cell::RefCell};
+
+
+type Memory=VirtualMemory<DefaultMemoryImpl>;
+type IdCell=Cell<u64,Memory>;
